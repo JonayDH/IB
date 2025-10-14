752106298c2ebbe91b8be9f8f4fa0087fe0598fc
usuario <alu0101791623@ull.edu.es>
2025-10-14 20:30:08 +0000

    Adición de los programas del capítulo Introduction

---
+++ Learning_to_program/Introduction/P56118-max_of_two/max.cc
@@ -0,0 +1,37 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Informática Básica 2025-2026
+  *
+  * @file max.cc
+  * @author Jonay Déniz Hernández  alu0101791623@ull.edu.es
+  * @date Oct 4 2025
+  * @brief A program that reads two numbers and prints their maximum.
+  * @bug There are no known bugs
+  * @see https://jutge.org/problems/P56118_en
+  *
+  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+  * as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+  */
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+  
+  long long int num1;
+  long long int num2;
+  long long int max;
+
+  std::cin >> num1;
+  std::cin >> num2;
+
+  max = std::fmax(num1,num2);
+  std::cout << max << std::endl;
+} 

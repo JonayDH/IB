2605fe32cf2302280c928f73889f2e8963ccc03f
JonayDH <alu0101791623@ull.edu.es>
2026-01-11 18:04:02 +0000

    Adicción de programas

---
+++ Puntito/fichero2/funciones.h
@@ -0,0 +1,8 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#include <string>
+
+void CambiarPalabras(const std::string& entrada, const std::string& palabra_buscar, const std::string& palabra_cambiar);
+
+#endif

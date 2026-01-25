c57b0670fc1320148f5870e99a8dc2a291065fb5
Jonay <alu0101791623@ull.edu.es>
2026-01-25 19:08:22 +0000

    Cambio de nombres

--- Puntito/fichero2/funciones.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNCIONES_H
-#define FUNCIONES_H
-
-#include <string>
-
-void CambiarPalabras(const std::string& entrada, const std::string& palabra_buscar, const std::string& palabra_cambiar);
-
-#endif
